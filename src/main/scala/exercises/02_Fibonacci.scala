3c5a6e5b120209219814c2ab37aa71c93c048728
Jeb Bearer <jbearer@hmc.edu>
2016-09-21 20:41:29 -0700

    Improve test coverage of exercises.Fibonacci.
    Added a properties check to ensure that for all n > 2, Fibonacci.fib
    satisfies the invariant fib(n) := fib(n - 1) + fib(n - 2).
    
    Testing large values of n returned by the generator revealed that
    the naive implementation of fib was too inefficient, so I implemented
    a linear time version which now runs in a reasonable amount of time.

--- src/main/scala/exercises/02_Fibonacci.scala
+++ src/main/scala/exercises/02_Fibonacci.scala
@@ -7,9 +7,13 @@ object Fibonacci {
    *    fib(2) = 1
    *    fib(n) = fib(n-1) + fib(n-2)
    */
-  def fib(n: Int): Int = if (n <= 2)
-    1
-  else
-    fib(n - 1) + fib(n - 2)
+  def fib(n: Int): Int = quick_fib(n)._1
+
+  private[this] def quick_fib(n: Int): (Int, Int) = if (n <= 1)
+    (1, 1)
+  else {
+    val prev = quick_fib(n - 1)
+    (prev._2, prev._1 + prev._2)
+  }
 
 }
--- src/test/scala/exercises/02_FibonacciTest.scala
+++ src/test/scala/exercises/02_FibonacciTest.scala
@@ -3,17 +3,37 @@ package exercises
 import Fibonacci.fib
 import org.scalatest.FunSuite
 import org.scalatest.Matchers
+import org.scalacheck.Properties
+import org.scalacheck.Prop.forAll
+import org.scalacheck.Gen
 
+/**
+ * FibonacciTest: check that Fibonacci.fib returns the correct value when
+ * evaluated at a few specific points, including the base cases 1 and 2.
+ */
 class FibonacciTest extends FunSuite with Matchers {
   test("fib(1)") {
     fib(1) should be (1)
   }
-  
+
   test("fib(2)") {
     fib(2) should be (1)
   }
-  
+
   test("fib(5)") {
     fib(5) should be (5)
-  } 
+  }
+}
+
+/**
+ * FibonacciCheck: ensure that Fibonacci.fib satisfies the invariant
+ *   fib(n) := fib(n - 1) + fib(n - 2)
+ * for many inputs.
+ */
+object FibonacciCheck extends Properties("Fibonacci") {
+  val inputs = Gen.choose(3, 1000)
+
+  property("fib") = forAll(inputs) { n ⇒
+    fib(n) == fib(n - 1) + fib(n - 2)
+  }
 }
