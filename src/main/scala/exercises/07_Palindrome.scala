c2d4db8e0251db7974b8c782fdcf8289b20832b8
Jeb Bearer <jbearer@hmc.edu>
2016-09-25 17:34:42 -0700

    Implement exercises.Palindrome

--- src/main/scala/exercises/07_Palindrome.scala
+++ src/main/scala/exercises/07_Palindrome.scala
@@ -8,6 +8,11 @@ object Palindrome {
    *  Hint: the Scala collections API is your friend
    *  http://docs.scala-lang.org/overviews/collections/overview.html 
    */
-  def isPalindrome(s: String): Boolean = throw new NotImplementedError
+  def isPalindrome(s: String): Boolean = clean(s.toList) == clean(s.toList).reverse
 
+  private[this] def clean(s: List[Char]): List[Char] = s.toList match {
+    case List()                => List()
+    case c :: cs if c.isLetter => c.toLower :: clean(cs)
+    case _ :: cs               => clean(cs)
+  }
 }
